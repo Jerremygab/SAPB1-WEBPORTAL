b5cc6205acc576d56aaca2b963c1d57f8382ca77
Jerremy Gabriel <jerremygabriel@gmail.com>
2024-01-23 17:27:53 +0800

    V-001

---
+++ modules/ap-credit-memo/proc/views/utilities/vw_itemsearch.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+include('../../../../../config/config.php');
+
+$txtItemSearch = $_GET['txtItemSearch'];
+
+
+    ?>
+    <thead>
+        <tr>
+            <th >#</th>
+            <th >Item Code</th>
+            <th >Item Name</th>
+            <th>Foreign Name</th>
+            <th>UoM Group</th>
+            <th>Inventory UoM</th>
+            <th class="d-none">Price</th>
+            <th class="d-none">Vendor</th>
+            <th class="d-none">UGP Entry</th>
+            <th class="d-none">UGP Code</th>
+            <th class="d-none">UGP Name</th>
+            <th class="d-none">Batch / Serial</th>
+            <th class="d-none">Whse Code</th>
+            <th class="d-none">Whse Name</th>
+        </tr>
+    </thead>
+    <tbody>
+
+
+
+						<?php
+							$itemno = 1;
+							$qry = odbc_exec($MSSQL_CONN, "USE [".$MSSQL_DB."]; SELECT DISTINCT
+																						T0.ItemCode, 
+																						T0.ItemName, 
+																						T0.FrgnName,
+																						T0.InvntryUom, 
+																						T0.CardCode, 
+																						T0.ManBtchNum,
+																						T0.ManSerNum,
+																						T1.ItmsGrpNam,
+																						CASE WHEN T2.Price = 0 THEN '' END AS Price,
+																						T4.UomEntry,
+																						T4.UomCode,
+																						T4.UomName,
+																						
+																						T0.DfltWH,
+																						T5.WhsName
+																						
+																						
+																							
+																						FROM OITM T0
+																						INNER JOIN OITB T1 ON T0.ItmsGrpCod = T1.ItmsGrpCod
+																						INNER JOIN ITM1 T2 ON T2.ItemCode = T0.ItemCode
+																						LEFT JOIN OPLN T3 ON T3.ListNum = T2.PriceList
+																						INNER JOIN OUOM T4 ON T4.UomEntry = T0.IUoMEntry
+																						LEFT JOIN OWHS T5 ON T5.WhsCode = T0.DfltWH
+																						
+																						WHERE T0.PrchseItem = 'Y' AND T0.FrozenFor = 'N'
+                                                                                        AND T0.ItemCode = '$txtItemSearch'
+
+                                                                                      	
+																						
+																						ORDER BY T0.ItemName ASC");
+								while (odbc_fetch_row($qry)) 
+								{
+									$BatchSerial = '';
+									if(odbc_result($qry, 'ManBtchNum') == 'Y'){
+										$BatchSerial = 'B';
+									}
+									else if(odbc_result($qry, 'ManSerNum') == 'Y'){
+										$BatchSerial = 'S';
+									}
+									else{
+										$BatchSerial = 'N';
+									}
+									echo '<tr class="">
+												<td>'.$itemno.'</td>
+												<td class="item-1">'.odbc_result($qry, 'ItemCode').'</td>
+												<td class="item-2" >'.odbc_result($qry, 'ItemName').'</td>
+												<td class="item-3 " >'.odbc_result($qry, 'FrgnName').'</td>
+												<td class="item-4 " >'.odbc_result($qry, 'ItmsGrpNam').'</td>
+												<td class="item-5 " >'.odbc_result($qry, 'InvntryUom').'</td>
+												<td class="item-6 hidden" >'.odbc_result($qry, 'Price').'</td>
+												<td class="item-7 hidden" >'.odbc_result($qry, 'CardCode').'</td>
+												<td class="item-8 hidden" >'.odbc_result($qry, 'UomEntry').'</td>
+												<td class="item-9 hidden" >'.odbc_result($qry, 'UomCode').'</td>
+												<td class="item-10 hidden" >'.odbc_result($qry, 'UomName').'</td>
+												<td class="item-11 hidden" >'.$BatchSerial.'</td>
+												<td class="item-12 hidden" >'.odbc_result($qry, 'DfltWH').'</td>
+												<td class="item-13 hidden" >'.odbc_result($qry, 'WhsName').'</td>
+											  </tr>';
+									$itemno++;	  
+								}
+								
+								odbc_free_result($qry);
+							
+
+						?>
+						</tbody>
+						<script>
+						
+						if ($.fn.dataTable.isDataTable('#tblFormSettings')) {
+							// Do something else since the dataTable's already initialized
+							
+							}
+							else {
+										$('#tblItem').dataTable({"bLengthChange": false, "searching": false});
+							}
+						</script>
+	<?php
+	odbc_free_result($qry);
+	
+?>
---
+++ modules/ap-downpayment-invoice/proc/views/utilities/vw_documentsearch.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+include('../../../../../config/config.php');
+
+$txtDocumentSearch = $_GET['txtDocumentSearch'];
+
+
+    ?>
+  	<thead>
+							<tr>
+								<th >#</th>
+								<th>Doc No.</th>
+								<th class='d-none'>Doc Entry</th>
+								<th>Posting Date</th>
+								<th class='d-none'>Customer Code</th>
+								<th >Customer Name</th>
+								<th>Remarks</th>
+								<th>Due Date</th>
+								<th>Total</th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php
+							$itemno = 1;
+							$qry = odbc_exec($MSSQL_CONN, "USE [".$MSSQL_DB."]; SELECT DISTINCT
+																						T0.DocNum, 
+																						T0.DocEntry,
+																						T0.CardCode, 
+																						T0.CardName,
+																						T0.Comments,
+																						T0.DocDate, 
+																						T0.DocDueDate, 
+																						T0.DocTotal
+																						
+																							
+																						FROM ODPO T0
+
+
+																						WHERE CAST(T0.DocNum AS NVARCHAR) = '$txtDocumentSearch'
+
+																						UNION ALL
+
+																					SELECT DISTINCT
+																						T0.DocNum, 
+																						T0.DocEntry,
+																						T0.CardCode, 
+																						T0.CardName,
+																						T0.Comments,
+																						T0.DocDate, 
+																						T0.DocDueDate, 
+																						T0.DocTotal
+																						
+																							
+																						FROM ODPO T0
+
+
+																						WHERE T0.CardCode = '$txtDocumentSearch'
+
+																						UNION ALL
+
+																						SELECT DISTINCT  
+																						T0.DocNum, 
+																						T0.DocEntry,
+																						T0.CardCode, 
+																						T0.CardName,
+																						T0.Comments,
+																						T0.DocDate, 
+																						T0.DocDueDate, 
+																						T0.DocTotal
+																						
+																							
+																						FROM ODPO T0
+
+
+																						WHERE T0.CardName = '$txtDocumentSearch'
+
+																						
+																						ORDER BY T0.DocNum ASC");
+								while (odbc_fetch_row($qry)) 
+								{
+									echo '<tr class="">
+												<td>'.$itemno.'</td>
+												<td class="item-1">'.odbc_result($qry, 'DocNum').'</td>
+												<td class="item-2 d-none">'.odbc_result($qry, 'DocEntry').'</td>
+												<td class="item-4 " >'.SAPDateFormater(odbc_result($qry, 'DocDate')).'</td>
+												<td class="item-3 d-none" >'.odbc_result($qry, 'CardCode').'</td>
+												<td class="item-6 " >'.odbc_result($qry, 'CardName').'</td>
+												<td class="item-8 " style="width: 160px;" >'.odbc_result($qry, 'Comments').'</td>
+												<td class="item-9 " >'.SAPDateFormater(odbc_result($qry, 'DocDueDate')).'</td>
+												<td class="item-5 text-right" >'.number_format(odbc_result($qry, 'DocTotal'),2).'</td>
+											  </tr>';
+									$itemno++;	  
+								}
+								
+								odbc_free_result($qry);
+							
+
+						?>
+						</tbody>
+						<script>
+							// var table = $('#myTable').DataTable();
+							if ($.fn.dataTable.isDataTable('#tblDoc')) {
+								$('#tblDoc').dataTable({"bLengthChange": true, "searching": false});
+							
+							}
+							else {	
+							
+							$('#tblDoc').dataTable({"bLengthChange": false, "searching": false});
+						
+								}
+									</script>
+	odbc_free_result($qry);
+	
+?>
