2a320d298a7aa5b86a443abba12354e1e1e3e3d1
Jerremygab <jerremygabriel@gmail.com>
2024-05-02 18:37:09 +0800

    V-003

--- modules/ap-downpayment-invoice/templates/ap-downpayment-invoice-document.php
+++ modules/ap-downpayment-invoice/templates/ap-downpayment-invoice-document.php
@@ -868,23 +868,94 @@ include '../../head.php' ;
           </div>
           <!--Body-->
           <div class="modal-body">
-                    <div class="row mb-3">
-                        <div class="col-sm-2" >
-                            <label for="inputEmail3" class=" col-form-label " style="color: black;" >Search</label>
-                        </div>
-                        <div class="col-4">                 
-                            <div class="input-group mb-1">
-                                <input type="text" id="txtCardCodeSearch" class="form-control" placeholder="" style="border-bottom-left-radius:5px; border-top-left-radius:5px;">
-                                <div class="input-group-append">
-                                    <button id="btnCardCodeSearch" class="btn btnGroup" type="button" data-mdb-ripple-color="dark"  style="background-color: #ADD8E6; "  >
-                                        <i class="fa fa-search" tabindex=0 style="color:blue "></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <table class="tblBP table table-striped table-bordered table-hover" id="tblBP" style="width: 100%">
                     
+                    <table class="tblBP table table-striped table-bordered table-hover" id="tblBP" style="width: 100%">
+                    <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Vendor Code</th>
+                                <th>Vendor Name</th>
+                                <th>Balance</th>
+                                <th>Contact Person</th>
+                                <th class="d-none">Payment Terms Code</th>
+                                <th class="d-none">Payment Terms Name</th>
+                                <th class="d-none">Tin Number</th>
+                                <th class="d-none">Contact Person Code</th>
+                                <th class="d-none">Currency</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+							$itemno = 1;
+							$qry = odbc_exec($MSSQL_CONN, "USE [".$MSSQL_DB."]; SELECT DISTINCT
+							--DISTINCT
+																						T0.CardCode, 
+																						T0.CardName,
+																						T0.Balance,
+																						T3.CntctCode,
+																						T0.CntctPrsn,
+																						T0.LicTradNum,
+																						T0.GroupNum,
+																						T0.Currency,
+																						T2.PymntGroup
+																						
+																						
+																						
+																						FROM OCRD T0
+																						LEFT JOIN CRD1 T1 ON T0.CardCode = T1.CardCode 
+																						LEFT JOIN OCTG T2 ON T2.GroupNum = T0.GroupNum
+																						LEFT JOIN OCPR T3 ON T3.Name = T0.CntctPrsn AND T0.CardCode = T3.CardCode 
+																						
+																						WHERE T0.CardType = 'S' AND T0.CardCode = '$txtCardCodeSearch'
+																						
+																						UNION ALL
+
+																						SELECT DISTINCT
+																						--DISTINCT
+																						T0.CardCode, 
+																						T0.CardName,
+																						T0.Balance,
+																						T3.CntctCode,
+																						T0.CntctPrsn,
+																						T0.LicTradNum,
+																						T0.GroupNum,
+																						T0.Currency,
+																						T2.PymntGroup
+																						
+																						
+																						
+																						FROM OCRD T0
+																						LEFT JOIN CRD1 T1 ON T0.CardCode = T1.CardCode 
+																						LEFT JOIN OCTG T2 ON T2.GroupNum = T0.GroupNum
+																						LEFT JOIN OCPR T3 ON T3.Name = T0.CntctPrsn AND T0.CardCode = T3.CardCode 
+																						
+																						WHERE T0.CardType = 'S' AND T0.CardName = '$txtCardCodeSearch'
+
+																						ORDER BY T0.CardCode ASC");
+								while (odbc_fetch_row($qry)) 
+								{
+									echo '<tr class="tableHover">
+												<td>'.$itemno.'</td>
+												<td class="item-1">'.odbc_result($qry, 'CardCode').'</td>
+												<td class="item-2">'.odbc_result($qry, 'CardName').'</td>
+												<td class="item-3 text-right">'.number_format(odbc_result($qry, 'Balance'),2).'</td>
+												<td class="item-4">'.odbc_result($qry, 'CntctPrsn').'</td>
+												<td class="item-5 d-none">'.odbc_result($qry, 'GroupNum').'</td>
+												<td class="item-6 d-none">'.odbc_result($qry, 'PymntGroup').'</td>
+												<td class="item-7 d-none">'.odbc_result($qry, 'LicTradNum').'</td>
+												<td class="item-8 d-none">'.odbc_result($qry, 'CntctCode').'</td>
+												<td class="item-9 d-none">'.odbc_result($qry, 'Currency').'</td>
+												
+											  </tr>';
+									$itemno++;	  
+								}
+								
+								odbc_free_result($qry);
+							
+
+						?>
+                        </tbody>
                     </table>
                 </div>
           <!--Footer-->
@@ -2309,6 +2380,11 @@ include '../../head.php' ;
     </script>
     <script>
     $('#tblGL').dataTable({
+        "bLengthChange": false,
+    });
+    </script>
+    <script>
+    $('#tblBP').dataTable({
         "bLengthChange": false,
     });
     </script>
